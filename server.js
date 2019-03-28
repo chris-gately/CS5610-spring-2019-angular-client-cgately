82c20bd5cdd71d2974c1747d119536769959eafe
Chris Gately <gately.c@husky.neu.edu>
2019-03-28 03:05:17 -0400

    Trying to deploy 2

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/CS5610-spring-2019-angular-client-cgately'));
+app.use(express.static(__dirname + '/dist/client-angular'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname+'/dist/CS5610-spring-2019-angular-client-cgately/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/client-angular/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
